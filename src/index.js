14733ad01afc43db34549a9f3e5c78b780cecf9e
jasonxuyang <jasonxuyang@gmail.com>
2022-04-18 03:40:51 -0400

    add img hover effects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 let main = document.getElementById("main");
+let sectionHero = document.getElementById("section_hero");
+let section1 = document.getElementById("section_1");
+let section2 = document.getElementById("section_2");
+let section3 = document.getElementById("section_3");
+let images = document.querySelectorAll("img");
+let imageCaption = document.getElementById("img_caption");
 let body = document.body;
 let parallaxElements = document.querySelectorAll(".parallax");
 let wrapperY = 0;
-let pointerPosX = 0;
-let pointerPosY = 0;
 let cursorX = 0;
 let cursorY = 0;
 let scrollY = 0;
@@ -13,7 +17,13 @@ const initPage = () => {
   body = document.body;
 
   window.addEventListener("scroll", scroll);
-  //   window.addEventListener("mousemove", findPointerPosition);
+  window.addEventListener("mousemove", findPointerPosition);
+  images.forEach((image) => {
+    image.addEventListener("mouseover", handleMouseEnter);
+    image.addEventListener("mouseleave", handleMouseLeave);
+  });
+  fdr.addEventListener("mouseover", handleMouseEnter);
+  fdr.addEventListener("mouseleave", handleMouseLeave);
   window.requestAnimationFrame(render);
 };
 
@@ -21,17 +31,43 @@ const scroll = () => {
   scrollY = window.scrollY;
 };
 
+const scrollTo = (section) => {
+  window.scrollBy(0, section.getBoundingClientRect().top);
+};
+
+const scrollToTop = () => scrollTo(sectionHero);
+const scrollToSection1 = () => {
+  window.scrollBy(0, section2.getBoundingClientRect().top - 128);
+};
+const scrollToSection2 = () => {
+  window.scrollBy(0, section3.getBoundingClientRect().top - 128);
+};
+
+const handleMouseEnter = (e) => {
+  imageCaption.textContent = e.target.alt;
+  imageCaption.style.opacity = "1";
+};
+
+const handleMouseLeave = (e) => {
+  imageCaption.style.opacity = "0";
+};
+
 const lerp = (a, b, n) => {
   return (1 - n) * a + n * b;
 };
 
-// const findPointerPosition = (e) => {
-//   try {
-//     pointerPosX = e.clientX - cursor.clientWidth / 2;
-//     pointerPosY = e.clientY - cursor.clientHeight / 2;
-//   } catch (error) {
-//     console.log(error);
-//   }
+const findPointerPosition = (e) => {
+  try {
+    imageCaption.style.top = e.clientY - imageCaption.clientHeight / 2 + "px";
+    imageCaption.style.left = e.clientX - imageCaption.clientWidth / 2 + "px";
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// const updateImageCaption = () => {
+//   imageCaption.style.top = cursorX;
+//   imageCaption.style.left = cursorY;
 // };
 
 // const updatePointerPosition = () => {
@@ -64,7 +100,7 @@ const render = () => {
     wrapperY = Math.floor(wrapperY * 100) / 100;
     main.style.transform = `translate(0px, -${wrapperY}px)`;
 
-    // updatePointerPosition();
+    // updateImageCaption();
     parallax();
 
     window.requestAnimationFrame(render);
